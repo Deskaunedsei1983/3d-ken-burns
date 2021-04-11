d4f2fd6ba7f71896e9d7de3005d8f505c289f246
Deskaunedsei1983 <stevemp83@gmail.com>
2021-04-11 18:40:22 +0200

    Update autozoom-long.py

--- autozoom-long.py
+++ autozoom-long.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 	})
 
 	npyResult = process_kenburns({
-		'fltSteps': numpy.linspace(0.0, .05, 200).tolist(),
+		'fltSteps': numpy.linspace(0.0, .1, 200).tolist(),
 		'objFrom': objFrom,
 		'objTo': objTo,
 		'boolInpaint': True
@@ -126,5 +126,5 @@ if __name__ == '__main__':
 #  
 #  
 
-	moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:] ], fps=25).write_videofile(arguments_strOut)
+	moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:] ], fps=30).write_videofile(arguments_strOut)
 # end
